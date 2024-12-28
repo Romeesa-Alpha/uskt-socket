ab6fab2f53baad202040de1be42dc6737b6b0627
Romeesa-Alpha <romeesaimran867@gmail.com>
2024-12-28 23:15:48 +0000

    new script add

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0f08f6b9c5b3f2d1a7c3e74a9d6b8a5'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def home():
-    return "Chatbot WebSocket Server is running!"
-
+# WebSocket event handler
 @socketio.on('message')
 def handle_message(data):
     print(f"Received message: {data}")
@@ -17,5 +14,30 @@ def handle_message(data):
     response = {"reply": f"Chatbot response to: {user_input}"}
     emit('response', response)
 
+# Run the Flask app with socketio support
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
+
+
+
+# from flask import Flask, render_template # type: ignore
+# from flask_socketio import SocketIO, emit # type: ignore
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'e0f08f6b9c5b3f2d1a7c3e74a9d6b8a5'
+# socketio = SocketIO(app, cors_allowed_origins="*")
+
+# @app.route('/')
+# def home():
+#     return "Chatbot WebSocket Server is running!"
+
+# @socketio.on('message')
+# def handle_message(data):
+#     print(f"Received message: {data}")
+#     user_input = data['message']
+#     # Generate chatbot response
+#     response = {"reply": f"Chatbot response to: {user_input}"}
+#     emit('response', response)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
