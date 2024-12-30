80cb8b40dc3593ee49c85b0c4faeed2c88fae324
Romeesa-Alpha <romeesaimran867@gmail.com>
2024-12-30 15:15:20 +0000

    libraries install

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = '12345678'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('connect')
